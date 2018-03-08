88349ef57ee80678dc31d3d72f5a4618824205f1
Andrey Alfaro <andreyalfaro@gmail.com>
2018-03-08 00:10:10 -0600

    Addes custom components theming support for material desing theme Improved the sidenav and the about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'aa-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
 })
 
 export class AboutComponent {
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -3,7 +3,6 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'aa-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
 })
 
 export class ExperienceComponent{
